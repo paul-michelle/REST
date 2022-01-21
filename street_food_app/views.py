583087ed6b51819ebe441874507f1da22306c598
paul-michelle <paulmichellegallery@gmail.com>
2022-01-21 04:07:25 +0300

    Add PUT method. Refactor controller functions

--- street_food_app/views.py
+++ street_food_app/views.py
@@ -1,3 +1,4 @@
+from typing import List, Optional
 import rest_framework.response
 import rest_framework.request
 from mongoengine.errors import DoesNotExist
@@ -22,22 +23,68 @@ from street_food_app.models import (
 
 
 def merge_data(sql: TicketSerializer, nosql: DeveloperSerializer) -> TicketInfo:
-    developer_data = {"developer": nosql.data}
+    developer_data = {"assigned_to": nosql.data}
     ticket_data = sql.data
     ticket_data.update(developer_data)
     return ticket_data
+
+
+def delete_unassigned_developer(developer) -> None:
+    developer_has_no_other_tickets = not (
+            Ticket.objects.filter(assigned_to=developer['github_account']).count() - 1
+    )
+    if developer_has_no_other_tickets:
+        developer.delete()
+
+
+def upsert_ticket(request, developer_requested: Optional[Developer] = None,
+                  ticket_requested: Optional[Ticket] = None) -> rest_framework.response.Response:
+    serializer_sql = None
+    serializer_nosql = None
+    developer_sent_info = request.data.pop('assigned_to')
+
+    if ticket_requested:
+        if developer_sent_info['github_account'] != developer_requested['github_account']:
+            delete_unassigned_developer(developer_requested)
+            serializer_nosql = DeveloperSerializer(data=developer_sent_info)
+        if developer_sent_info['github_account'] == developer_requested['github_account']:
+            serializer_nosql = DeveloperSerializer(data=developer_sent_info, instance=developer_requested)
+        serializer_sql = TicketSerializer(data=request.data, instance=ticket_requested)
+
+    if not ticket_requested:
+        serializer_sql = TicketSerializer(data=request.data)
+        serializer_nosql = DeveloperSerializer(data=developer_sent_info)
+
+    serializer_sql.is_valid()
+    serializer_nosql.is_valid()
+
+    if serializer_sql.is_valid() and serializer_nosql.is_valid():
+        serializer_sql.save(assigned_to=developer_sent_info['github_account'])
+        try:
+            Developer.objects.get(github_account=developer_sent_info['github_account'])
+        except DoesNotExist:
+            serializer_nosql.save()
+
+        ticket_data = merge_data(serializer_sql, serializer_nosql)
+        return rest_framework.response.Response(data=ticket_data, status=status.HTTP_201_CREATED)
+
+    errors = [serializer_sql.errors, serializer_nosql.errors]
+    return rest_framework.response.Response(data=errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
 def ticket_list_all_or_create_one(request: HttpRequest) -> rest_framework.response.Response:
     request: rest_framework.request.Request
 
+    if request.method == 'POST':
+        return upsert_ticket(request)
+
     if request.method == 'GET':
-        data = []
+        data: List[TicketInfo]
+        data = list()
+
         there_are_tickets = Ticket.objects.count()
-
         if there_are_tickets:
-
             for ticket in Ticket.objects.all():
                 serializer_sql = TicketSerializer(ticket)
                 try:
@@ -45,34 +92,10 @@ def ticket_list_all_or_create_one(request: HttpRequest) -> rest_framework.respon
                 except DoesNotExist:
                     break
                 serializer_nosql = DeveloperSerializer(developer_info)
-
-                developer_data = {"developer": serializer_nosql.data}
-                ticket_data = serializer_sql.data
-                ticket_data.update(developer_data)
+                ticket_data = merge_data(serializer_sql, serializer_nosql)
                 data.append(ticket_data)
 
         return rest_framework.response.Response(data=data, status=status.HTTP_200_OK)
-
-    if request.method == 'POST':
-        developer_info = request.data.pop('assigned_to')
-        serializer_sql = TicketSerializer(data=request.data)
-        serializer_nosql = DeveloperSerializer(data=developer_info)
-
-        serializer_sql.is_valid()
-        serializer_nosql.is_valid()
-
-        if serializer_sql.is_valid() and serializer_nosql.is_valid():
-            serializer_sql.save(assigned_to=developer_info['github_account'])
-            try:
-                Developer.objects.get(github_account=developer_info['github_account'])
-            except DoesNotExist:
-                serializer_nosql.save()
-
-            ticket_data = merge_data(serializer_sql, serializer_nosql)
-            return rest_framework.response.Response(data=ticket_data, status=status.HTTP_201_CREATED)
-
-        errors = [serializer_sql.errors, serializer_nosql.errors]
-        return rest_framework.response.Response(data=errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -86,9 +109,7 @@ def ticket_read_update_delete_one(request: HttpRequest, pk: int) -> rest_framewo
     developer_requested = Developer.objects.get(github_account=developer_github)
 
     if request.method == 'DELETE':
-        developer_requested_has_no_other_tickets = not (Ticket.objects.filter(assigned_to=developer_github).count() - 1)
-        if developer_requested_has_no_other_tickets:
-            developer_requested.delete()
+        delete_unassigned_developer(developer_requested)
         ticket_requested.delete()
         return rest_framework.response.Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -99,6 +120,4 @@ def ticket_read_update_delete_one(request: HttpRequest, pk: int) -> rest_framewo
         return rest_framework.response.Response(data=ticket_data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
-        return rest_framework.response.Response(status=status.HTTP_204_NO_CONTENT)
-
-    return rest_framework.response.Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return upsert_ticket(request, developer_requested, ticket_requested)
